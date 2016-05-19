5e78edb6181c8fd93a38d590e9f9cbe84986f374
Juriy Bura <juriy.bura@gmail.com>
2016-05-19 22:54:54 +0300

    Added single auction data

--- server/api/routers/auction.js
+++ server/api/routers/auction.js
@@ -13,6 +13,16 @@ module.exports = () => {
 			res.json(auctions);
 		});
 
+	router
+		.route('/:aucId')
+		.get((req, res) => {
+			const aucId = +req.params.aucId;
+
+			const auctionDetails = auctions.find((auc) => auc.id === aucId);
+			res.json(auctionDetails);
+		});
+
+
 	router.use('/:aucId/lot', lotsRouter());
 
 	return router;
